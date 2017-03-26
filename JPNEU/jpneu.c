5afa581619c33238ec303869de42de636facb118
Renato Uchoa <renatoubra@gmail.com>
2017-03-26 10:06:53 -0300

    Solução em C para o problema JPNEU
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

---
+++ JPNEU/jpneu.c
@@ -0,0 +1,16 @@
+/**
+ * http://br.spoj.com/problems/JPNEU/
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int desejada, lida;
+
+    scanf("%d%d", &desejada, &lida);
+
+    printf("%d", desejada - lida);
+
+    return 0;
+}
